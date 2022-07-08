0af4d711342ceeb01d64cc17e163f8652417edbb
s1290012 <s1290012@u-aizu.ac.jp>
2022-07-08 12:17:09 +0900

    HoT: initial revision

---
+++ task4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+//Head or Tail game
+
+int main(){
+  srand(time(NULL));
+  int i,hc=0,tc=0,r;
+  char H[] = "Heads", T[] = "Tails";
+  printf("Tossing a coin. . .\n");
+
+  for(i = 1; i <= 3; i++){
+    r = rand();
+    printf("Round %d: ",i);
+    if(r==0){
+      printf("%s\n",H);
+      hc++;
+    }
+    else if(r%2 == 0){
+      printf("%s\n",H);
+      hc++;
+    }
+    else{
+      printf("%s\n",T);
+      tc++;
+    }
+  }
+  printf("Heads: %d, Tails: %d\n",hc,tc);
+  return 0;
+}
